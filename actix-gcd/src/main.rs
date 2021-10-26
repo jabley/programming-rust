b0f549d78966dec5f9951b9132fbcda94ca9c418
James Abley <james.abley@gmail.com>
2021-10-26 15:48:16 +0100

    Initial commit for actix-gcd
    Web server (with upgraded version of actix_web) which returns HTML.

---
+++ actix-gcd/src/main.rs
@@ -0,0 +1,25 @@
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let server = HttpServer::new(||
+        App::new()
+            .route("/", web::get().to(get_index))
+    );
+
+    println!("Serving on http://localhost:3000...");
+    server.bind("127.0.0.1:3000")?.run().await
+}
+
+async fn get_index() -> HttpResponse {
+    HttpResponse::Ok().content_type("text/html").body(
+        r#"
+        <title>GCD Calculator</title>
+        <form action="/gcd" method=post>
+            <input type=text name=n />
+            <input type=text name=m />
+            <button type=submit>Compute GCD</button>
+        </form>
+        "#,
+    )
+}
