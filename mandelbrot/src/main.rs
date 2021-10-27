913c4ac6d98ca8509f66d368d6f8c83ab2d5ea8a
James Abley <james.abley@gmail.com>
2021-10-27 13:31:16 +0100

    Add write_image with new dependency

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,3 +1,4 @@
+use image::{png::PNGEncoder, ColorType};
 use num::Complex;
 use std::{fs::File, str::FromStr};
 
@@ -96,6 +97,24 @@ fn render(
             }
         }
     }
+}
+
+fn write_image(
+    filename: &str,
+    pixels: &[u8],
+    bounds: (usize, usize),
+) -> Result<(), std::io::Error> {
+    let output = File::create(filename)?;
+
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(
+        &pixels,
+        bounds.0 as u32,
+        bounds.1 as u32,
+        ColorType::Gray(8),
+    )?;
+
+    Ok(())
 }
 
 #[test]
